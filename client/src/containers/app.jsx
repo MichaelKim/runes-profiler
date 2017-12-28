1516148e6b2a9dac7e7c163ae2d2d26f083a9e59
Michael Kim <micklepickle98@gmail.com>
2017-12-28 09:43:20 -0500

    Update footer

--- client/src/containers/app.jsx
+++ client/src/containers/app.jsx
@@ -90,6 +90,9 @@ class App extends React.Component {
 		    		</React.Fragment> :
 		    		<Loader />
 		    	}
+		    	<div className='footer'>
+					<p className='disclaimer'>Runes Profiler isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends.<br/>League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</p>
+				</div>
 		    </React.Fragment>
 		);
 	}
