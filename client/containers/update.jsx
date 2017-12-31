7b6431c480f6096d2facbd5e87d2d1552069dcbf
Michael Kim <micklepickle98@gmail.com>
2017-12-31 18:52:43 -0500

    Remove update button

--- client/containers/update.jsx
+++ client/containers/update.jsx
@@ -3,50 +3,22 @@ import PropTypes from 'prop-types';
 
 const Months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-class Update extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			showUpdateError: false
-		};
+const Update = ({ lastUpdated })  => {
+	let dateString = 'Never';
+	if (lastUpdated) {
+		let d = new Date(lastUpdated);
+		dateString = Months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' +
+			d.getHours() + ':' + d.getMinutes().toString().padStart(2, '0');
 	}
-
-	checkUpdate() {
-		console.log(Date.now() - this.props.lastUpdated);
-		if (!this.props.lastUpdated || Date.now() - this.props.lastUpdated > 1000 * 60 * 5) {
-			this.props.onUpdate();
-		}
-		else {
-			this.setState({ showUpdateError: true });
-			setTimeout(() => this.setState({ showUpdateError: false }), 2000);
-		}
-	}
-
-	render() {
-		let dateString = 'Never';
-		if (this.props.lastUpdated) {
-			let d = new Date(this.props.lastUpdated);
-			dateString = Months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' +
-				d.getHours() + ':' + d.getMinutes().toString().padStart(2, '0');
-		}
-		
-		return (
-			<div className='update-box'>
-				{/*<button
-					className='update-button'
-					onClick={() => this.checkUpdate()}
-				>Update</button>*/}
-				<p className='last-updated'>Last updated:<br/>{dateString}</p>
-				<p className='invalid-update' style={{
-					opacity: this.state.showUpdateError ? '1' : '0'
-				}}>Updated too soon, try again</p>
-		    </div>
-		);
-	}
+	
+	return (
+		<div className='update-box'>
+			<p className='last-updated'>Last updated:<br/>{dateString}</p>
+	    </div>
+	);
 }
 
 Update.propTypes = {
-	onUpdate: PropTypes.func.isRequired,
 	lastUpdated: PropTypes.number.isRequired
 };
 
